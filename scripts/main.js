2de09cc75ae23215acd9664793357d5cbdc6ff7d
PS <psenfft@stud.hs-heilbronn.de>
2021-06-29 18:32:47 +0200

    added bootstrap work on detail site

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,25 @@ function Film(id, name, picture) {
     this.id = id;
     this.name = name;
     this.picture = picture;
-
-    function LoadDetails() {
-        //todo implement details page
-    }
 }
+
+//todo implement details page
+
 
 function FilmList(title) {
     this.title = title;
-    this.films = [
+    this.films = ko.observableArray([//todo remove fix listand use var films instead
         new Film(0, "Vikings", "../Resources/example01.jpg"),
         new Film(1, "The Witcher", "../Resources/example02.jpg"),
         new Film(2, "Mobile Suit Gundam", "../Resources/example03.jpg"),
         new Film(3, "Star Trek Discovery", "../Resources/example04.jpg"),
         new Film(4, "KIN", "../Resources/example05.jpg"),
         new Film(5, "Unser Planet", "../Resources/example06.jpg")
-    ];
+    ]);
+
+    this.loadMediaDetails = function () {
+        alert("Hey, ich bin " + self.title + " ");
+    }
 }
 
 //Temporary method to insert new areas for a film list
