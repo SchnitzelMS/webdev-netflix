c3e80248dbddbafcbb12c2083524855f1b364946
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2021-06-29 23:37:20 +0200

    first version of an details page
    -added a film repository where new entries can be added via json
    -modal for film details
    -javascript to open modal and build list from json to objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,14 @@
-function Film(id, name, director, year, description, actor, picture) {
-    this.id = id;
-    this.name = name;
-    this.director = director;
-    this.year = year;
-    this.description = description;
-    this.actor = actor;
-    this.picture = picture;
-}
-
-//todo implement details page
-
-
-function FilmList(title) {
-    this.title = title;
-    this.films = ko.observableArray([//todo remove fix listand use var films instead
-        new Film(0, "Vikings", "../Resources/example01.jpg"),
-        new Film(1, "The Witcher", "../Resources/example02.jpg"),
-        new Film(2, "Mobile Suit Gundam", "../Resources/example03.jpg"),
-        new Film(3, "Star Trek Discovery", "../Resources/example04.jpg"),
-        new Film(4, "KIN", "../Resources/example05.jpg"),
-        new Film(5, "Unser Planet", "../Resources/example06.jpg")
-    ]);
-
-    this.loadMediaDetails = function () {
-        alert("Hey, ich bin " + self.title + " ");
-    }
-}
-
-//Temporary method to insert new areas for a film list
-function LoadFilmLists() {
-    let list = [];
-    list.push(new FilmList("Leider geil"));
-    list.push(new FilmList("Thriller"));
-    list.push(new FilmList("Neu bei uns"));
-    list.push(new FilmList("Filled"));
-    return list;
-}
-
 //Main ViewModel
 function AppViewModel() {
-    this.film_list = LoadFilmLists();
+    var self = this;
+    self.film_list = ko.mapping.fromJS(GetFilms());
+    self.current = ko.observable(null);
+    self.loadDetails = function (film) {
+        self.current(film);
+        var modal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#myModal'));
+        modal.show();
+    }
 }
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
-
---
+++ scripts/repository.js
@@ -0,0 +1,90 @@
+// This is our film repository.
+// If you want more films shown on the website, add them to the list below and refresh the page
+function GetFilms() {
+    return [
+        {
+            title: "Neue Filme",
+            films: [
+                {
+                    id: "1",
+                    name: "2010 Das Jahr, in dem wir Kontakt aufnahmen",
+                    director: "Keine Ahnung",
+                    year: "19xx",
+                    description: "Wir fliegen wieder zum Jupiter oder so",
+                    actor: "Gab ein paar",
+                    picture: "../Resources/example02.jpg"
+                },
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example01.jpg"
+                }]
+        },
+        {
+            title: "Derzeit beliebt",
+            films: [
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example03.jpg"
+                },
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example04.jpg"
+                }]
+        },
+        {
+            title: "Nochmal ansehen",
+            films: [
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example05.jpg"
+                },
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example06.jpg"
+                },
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example05.jpg"
+                },
+                {
+                    id: "1",
+                    name: "dsa",
+                    director: "dsafg",
+                    year: "2021",
+                    description: "",
+                    actor: "",
+                    picture: "../Resources/example04.jpg"
+                }]
+        }
+    ];
+}
