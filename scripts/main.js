53b0cc31cf34a62064cf9ebe0132e251e3e52749
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2021-07-01 14:23:16 +0200

    bug fixes and imrpvements
    -modal now loads details page
     and more..

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,28 @@
 //Main ViewModel
 function AppViewModel() {
     let self = this;
-    self.film_list = ko.mapping.fromJS(GetFilms());
+    self.film_list = ko.observableArray();
     self.current = ko.observable(null);
     self.infoEnabled = ko.observable(false);
 
+    self.loadMainPage = function () {
+        self.infoEnabled(false);
+        self.current(null);
+        self.film_list = ko.mapping.fromJS(GetFilms());
+    }
+
     self.loadDetails = function (film) {
         self.current(film);
-        let modal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#myModal'));
+        let modal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#film-info-modal'));
         modal.show();
     }
 
-    self.enableInfo = function (film) {
-        self.current(film);
+    self.loadDetailsPage = function () {
+        self.film_list(null);
         self.infoEnabled(true);
     }
 
-    self.disableInfo = function() {
-        self.infoEnabled(false);
-    }
+    self.loadMainPage();
 }
 
 // Activates knockout.js
